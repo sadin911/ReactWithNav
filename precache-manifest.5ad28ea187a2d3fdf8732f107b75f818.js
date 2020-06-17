c68558726413724c5252661308a5383e1acc8e69
chonlatid.d <chonlatid.d@chanwanich.com>
2020-06-17 14:16:06 +0700

    Updates

---
+++ precache-manifest.5ad28ea187a2d3fdf8732f107b75f818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2839b9169894bca69303792855b08b9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fff9b7733c5eb7faa62",
+    "url": "/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "4e062b5957bb3d28ec5d",
+    "url": "/static/js/2.73552b3f.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/static/js/2.73552b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fff9b7733c5eb7faa62",
+    "url": "/static/js/main.727f2273.chunk.js"
+  },
+  {
+    "revision": "819676dd54ef13679437",
+    "url": "/static/js/runtime-main.35a802d6.js"
+  },
+  {
+    "revision": "d61546cda03af82f672458cb776c67a1",
+    "url": "/static/media/boatImage.d61546cd.jpg"
+  }
+]);
